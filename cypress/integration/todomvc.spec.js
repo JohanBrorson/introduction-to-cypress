373d35c35d9ab233b0b06d4fd7eaadd74ad11b89
Johan Brorson <johan_brorson@hotmail.com>
2019-09-17 14:57:15 +0200

    Chapter 3: Accessing elements
    Chapter 3: Accessing elements and interacting with them.

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,5 +1,8 @@
 /// <reference types="cypress" />
 
-it('should navigate to the TodoMVC app', () => {
+it('should add a new todo to the list', () => {
   cy.visit('http://todomvc-app-for-testing.surge.sh/');
+  cy.get('.new-todo', {timeout: 6000}).type("Learn Cypress{enter}");
+  cy.get('.toggle').click();
+  cy.contains('Clear').click();
 });

605d88ae9852cb7638544027b6d1d82acbd74b90
Johan Brorson <johan_brorson@hotmail.com>
2019-09-18 10:40:56 +0200

    Chapter 7: Page objects in Cypress

--- cypress/integration/todomvc-actions.spec.js
+++ cypress/integration/todomvc-actions.spec.js
@@ -1,24 +1,38 @@
 /// <reference types="cypress" />
 
-describe('todo actions', () => {
+import {
+  navigate,
+  addTodo,
+  getTodoList,
+  getToggle,
+  toggleTodo,
+  clearCompletedTodos,
+  getTodoTextItem
+} from "../page-objects/todo-page";
+
+describe("todo actions", () => {
   beforeEach(() => {
-    cy.visit('http://todomvc-app-for-testing.surge.sh/');
-    cy.get('.new-todo', {timeout: 6000}).type("Learn Cypress{enter}");
+    navigate();
+    addTodo("Learn Cypress");
   });
 
-  it('should add a new todo to the list', () => {
-    cy.get('label').should('have.text', 'Learn Cypress');
-    cy.get('.toggle').should('not.be.checked');
+  it("should add a new todo to the list", () => {
+    getTodoTextItem(0).should("have.text", "Learn Cypress");
+    getToggle().should("not.be.checked");
   });
 
-  it('should mark a todo as completed', () => {
-    cy.get('.toggle').click();
-    cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
+  it("should mark a todo as completed", () => {
+    toggleTodo(0);
+    getTodoTextItem(0).should(
+      "have.css",
+      "text-decoration-line",
+      "line-through"
+    );
   });
 
-  it('should clear completed todos', () => {
-    cy.get('.toggle').click();
-    cy.contains('Clear').click();
-    cy.get('.todo-list').should('not.have.descendants', 'li');
+  it("should clear completed todos", () => {
+    toggleTodo(0);
+    clearCompletedTodos();
+    getTodoList().should("not.have.descendants", "li");
   });
 });
--- cypress/integration/todomvc-filtering.spec.js
+++ cypress/integration/todomvc-filtering.spec.js
@@ -1,26 +1,36 @@
 /// <reference types="cypress" />
 
-describe('filtering', () => {
+import {
+  navigate,
+  addTodo,
+  getTodoListItems,
+  toggleTodo,
+  showAllTodos,
+  showOnlyActiveTodos,
+  showOnlyCompletedTodos
+} from "../page-objects/todo-page";
+
+describe("filtering", () => {
   beforeEach(() => {
-    cy.visit('http://todomvc-app-for-testing.surge.sh/');
-    cy.get('.new-todo').type("Learn Cypress{enter}");
-    cy.get('.new-todo').type("Learn WebdriverIO{enter}");
-    cy.get('.new-todo').type("Learn Puppeteer{enter}");
-    cy.get('.todo-list li:nth-child(2) .toggle').click();
+    navigate();
+    addTodo("Learn Cypress");
+    addTodo("Learn WebdriverIO");
+    addTodo("Learn Puppeteer");
+    toggleTodo(1);
   });
 
   it('should filter "Active" correctly', () => {
-    cy.contains('Active').click();
-    cy.get('.todo-list li').should('have.length', 2);
+    showOnlyActiveTodos();
+    getTodoListItems().should("have.length", 2);
   });
 
   it('should filter "Completed" correctly', () => {
-    cy.contains('Completed').click();
-    cy.get('.todo-list li').should('have.length', 1);
+    showOnlyCompletedTodos();
+    getTodoListItems().should("have.length", 1);
   });
 
   it('should filter "All" correctly', () => {
-    cy.contains('All').click();
-    cy.get('.todo-list li').should('have.length', 3);
+    showAllTodos();
+    getTodoListItems().should("have.length", 3);
   });
 });
